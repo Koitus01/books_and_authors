c8e776a7de5c99e2ef20da5e2c4f5d718239f9c0
Артём Быков <a.bykov@sprinthost.ru>
2023-04-18 23:07:55 +0300

    add flocks to book create

--- src/UseCase/CreateBook.php
+++ src/UseCase/CreateBook.php
@@ -5,15 +5,34 @@ namespace App\UseCase;
 use App\DTO\CreateBookDTO;
 use App\Entity\Book;
 use App\Exceptions\DuplicateBookException;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Lock\LockFactory;
 
 class CreateBook extends BaseUseCase
 {
+    private LockFactory $factory;
+
     /**
-     * TODO: create authors here???
+     * @param EntityManagerInterface $entityManager
+     * @param LockFactory $factory
+     */
+    public function __construct(EntityManagerInterface $entityManager, LockFactory $factory)
+    {
+        $this->factory = $factory;
+        parent::__construct($entityManager);
+    }
+
+
+    /**
      * @throws DuplicateBookException
      */
     public function execute(CreateBookDTO $DTO): Book
     {
+        // Do it through lock with book title name for preventing add book with same title and ISBN
+        // or same title and publishing year
+        $lock = $this->factory->createLock(str_replace(' ', '', $DTO->title));
+        $lock->acquire(true);
+
         $bookRepository = $this->entityManager->getRepository(Book::class);
         $sameTitleAndISBNBook = $bookRepository->findBy([
             'isbn' => $DTO->isbn->value(),
@@ -42,6 +61,8 @@ class CreateBook extends BaseUseCase
 
         $this->entityManager->persist($book);
         $this->entityManager->flush();
+
+        $lock->release();
 
         return $book;
     }
