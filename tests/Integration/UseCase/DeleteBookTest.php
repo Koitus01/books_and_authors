c8c4bb4318f1aa1174bdfc3ed002b853a276050e
Артём Быков <a.bykov@sprinthost.ru>
2023-04-18 23:34:19 +0300

    add testExecuteNotDeleteAuthor to DeleteBookTest

--- tests/Integration/UseCase/DeleteBookTest.php
+++ tests/Integration/UseCase/DeleteBookTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\Integration\UseCase;
 
+use App\Entity\Author;
 use App\Entity\Book;
 use App\Tests\Integration\BaseIntegration;
 use App\UseCase\DeleteBook;
@@ -24,6 +25,35 @@ class DeleteBookTest extends BaseIntegration
         $result = $this->doctrine->getRepository(Book::class)->find($book->getId());
 
         $this->assertEmpty($result);
+    }
+
+    public function testExecuteNotDeleteAuthor()
+    {
+        /** @var DeleteBook $db */
+        $db = $this->container->get(DeleteBook::class);
+        $manager = $this->doctrine->getManager();
+        $author = new Author();
+        $author->setFirstName('Viktor')->setSecondName('Hugo');
+        $this->doctrine->getManager()->persist($author);
+        $this->doctrine->getManager()->flush();
+        $book = new Book();
+        $book
+            ->setTitle($this->title())
+            ->setIsbn($this->ISBN())
+            ->setPublishing($this->publishing())
+            ->addAuthor($author);
+        $manager->persist($author);
+        $manager->persist($book);
+        $manager->flush();
+        $manager->clear();
+
+        $db->execute($book->getId());
+        $bookResult = $this->doctrine->getRepository(Book::class)->find($book->getId());
+        $authorResult = $this->doctrine->getRepository(Author::class)->find($author->getId());
+
+        $this->assertEmpty($bookResult);
+        $this->assertNotEmpty($authorResult);
+
     }
 
     public function testExecuteNonExistentWillThrow()
